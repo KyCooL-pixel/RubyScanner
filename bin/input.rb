eca3a58593f5ee3cccfd3fade02fb3c39db09ae7
Kah Yeek <kycheok2007@gmail.com>
2023-06-16 12:17:45 +0800

    warn: remove these

--- bin/input.rb
+++ bin/input.rb
@@ -1,4 +1,9 @@
-if sum = 10
-    hello = "<Hello>, world!"
-    sum += 1r
+# !/usr/bin/ruby –w
+$i = 0
+$num = 0
+time1 = Time.new
+while $i < $num do
+	puts ("Inside the loop i = #i")
 end
+puts "Current Time : " + time1.inspect
+
